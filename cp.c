68cb8676c145d18f64f4d9e13991d201320c3837
maruf6890 <137112466+maruf6890@users.noreply.github.com>
2023-10-04 19:56:38 +0600

    Update cp.c

--- cp.c
+++ cp.c
@@ -13,18 +13,7 @@ int main()
          scanf("%lld",&a[i]);
          
       }
-      /* for(int i=0; i<n;  i++){
-          if(0<= b[i]){
-            if (b[i]<=1000000000000000000)
-            {
-               b[i]= a[i];
-            }
-            
-             
-         }
-      }
- */
-      long long int mul=1;
+       long long int mul=1;
       for(int i=0; i<n;  i++){
          if(0<= a[i] && a[i]<=1000000000000000000){
             mul = mul*a[i];
