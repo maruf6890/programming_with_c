00e49d77a4985e336697cb3cfe621fdb2c2e728d
maruf6890 <137112466+maruf6890@users.noreply.github.com>
2023-10-05 12:23:42 +0600

    Update cp.c

--- cp.c
+++ cp.c
@@ -34,3 +34,35 @@ int main()
 
   return 0; 
    }
+#include <stdio.h>
+
+int main() {
+    int N,M,O,a,b,c,d,e,f,num=6 ; 
+    switch(N){
+        case 0:a=0,b=0, c=0;
+        case 1:a=1,b=0, c=0;
+        case 2:a=0,b=2, c=0;
+        case 3:a=1,b=2, c=0;
+        case 4:a=0,b=0, c=4;
+        case 5:a=1,b=0, c=4;
+        case 6:a=0,b=2, c=4;
+        case 7:a=1,b=2,c=4;
+    }
+    switch(M){
+        case 0:d=0,e=0, f=0;
+        case 1:d=1,e=0, f=0;
+        case 2:d=0,e=2, f=0;
+        case 3: d=1,e=2, f=0;
+        case 4:d=0,e=0, f=4;
+        case 5:d=1,e=0, f=4;
+        case 6:d=0,e=2, f=4;
+        case 7:d=1,e=2,f=4;
+    }
+     num[0]= a;  num[1]= b;  num[2]= c; num[3]= d; num[4]= e,num[5]=f;
+     
+    
+    
+    
+
+    return 0;
+}
